f8f56528e2c59c88088cfe0beb9463e32aec413a
Krzysztof Szabelski <kszabelski@gmail.com>
2014-12-29 14:51:12 +0100

    Empty MVC app template wit nuget enabled

---
+++ AspnetWithTypeScriptSampleApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace AspnetWithTypeScriptSampleApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ AspnetWithTypeScriptSampleApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AspnetWithTypeScriptSampleApp.MvcApplication" Language="C#" %>
