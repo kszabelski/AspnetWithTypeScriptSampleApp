49a3ac0e4089e47267ce192713bede656e711abc
Krzysztof Szabelski <kszabelski@gmail.com>
2014-12-29 15:01:56 +0100

    Sample TypeScript app in MVC structure

---
+++ AspnetWithTypeScriptSampleApp/Scripts/clock.js
@@ -0,0 +1,30 @@
+﻿var Clock = (function () {
+    function Clock(element) {
+        var _this = this;
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.stopButton = document.createElement("button");
+        this.stopButton.textContent = "Stop";
+        this.stopButton.onclick = function () {
+            return clearTimeout(_this.timerToken);
+        };
+        this.element.appendChild(this.stopButton);
+        this.span.innerText = new Date().toUTCString();
+    }
+    Clock.prototype.start = function () {
+        var _this = this;
+        this.timerToken = setInterval(function () {
+            return _this.span.innerHTML = new Date().toUTCString();
+        }, 500);
+    };
+    return Clock;
+})();
+
+window.onload = function () {
+    var el = document.getElementById('content');
+    var greeter = new Clock(el);
+    greeter.start();
+};
+//# sourceMappingURL=clock.js.map
---
+++ AspnetWithTypeScriptSampleApp/Scripts/clock.ts
@@ -0,0 +1,28 @@
+﻿class Clock {
+    element: HTMLElement;
+    span: HTMLElement;
+    stopButton: HTMLElement;
+    timerToken: number;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+        this.element.innerHTML += "The time is: ";
+        this.span = document.createElement('span');
+        this.element.appendChild(this.span);
+        this.stopButton = document.createElement("button");
+        this.stopButton.textContent = "Stop";
+        this.stopButton.onclick = () => clearTimeout(this.timerToken);
+        this.element.appendChild(this.stopButton);
+        this.span.innerText = new Date().toUTCString();
+    }
+
+    start() {
+        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    }
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+    var greeter = new Clock(el);
+    greeter.start();
+};
